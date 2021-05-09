00e0b0de64baac1f2dc7c52b1ad08ec00c69cdab
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-09 18:40:18 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,19 @@
+a_122 = '''vicky'''
+# a = 'vicky'
+# a = "vicky" 
+#we can write in double quot or single quotes or tripple quotes
+b = 45
+c = 45.22
+d = True
+# d = None
+# Printing the variables
+print(a_122)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a_122))
+print(type(b))
+print(type(c))
+print(type(d))
