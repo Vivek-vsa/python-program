2fb2e5cc741d4814aa2873364b976950f2a8ab24
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-11 18:12:03 +0530

    basic string

---
+++ strings.py
@@ -0,0 +1,6 @@
+# b = "vicky's" # --> Use this if you have single quotes in your strings
+# b = 'vicky"s' # --> Use this if you have double quotes in your strings
+b = '''vicky"s and  
+          vicky's'''      # --> Use this if you have single & double quotes in your strings
+print(b)
+print(type(b))
