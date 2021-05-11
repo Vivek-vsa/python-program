b2ea286ee6fe7e8d4048740d1f06aab956cc392c
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-11 18:19:03 +0530

    FormattedLetter.py

---
+++ formattedLetter.py
@@ -0,0 +1,5 @@
+letter = "Dear Vivek, This Python repository is nice! Thanks!"
+print(letter)
+
+formatted_letter = "Dear Vivek,\n\tThis Python repository is nice!\nThanks!"
+print(formatted_letter)
