ca9621a984461f1d3aaf23655bf91dfd7e8e8629
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-11 18:12:43 +0530

    Functions in String

---
+++ stringFunctions.py
@@ -0,0 +1,9 @@
+story = "once upon a time there was a developer named vicky who uploaded python course with notes"
+
+# String Functions
+print("characters",len(story))  #calculates the characters in the string
+print(story.endswith("notes"))  #it gives the boolean value of statement related to  string
+print(story.count("c"))         #it counts the particular character repetation number
+print(story.capitalize())       # Capitalise the starting character  of the string
+print(story.find("upon"))       #gives the index value searched word
+print(story.replace("vicky", "CodeWithVicky"))  #replaces the value of word
