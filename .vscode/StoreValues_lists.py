c08378b361101d0fc3a097ba339a84ee76d5f381
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-12 18:42:12 +0530

    StoreValues_lists.py

---
+++ .vscode/StoreValues_lists.py
@@ -0,0 +1,10 @@
+p1 = input("Enter Player name 1: ")
+p2 = input("Enter Player name 2: ")
+p3 = input("Enter Player name 3: ")
+p4 = input("Enter Player name 4: ")
+p5 = input("Enter Player name 5: ")
+p6 = input("Enter Player name 6: ")
+p7 = input("Enter Player name 7: ")
+
+myPlayerList = [p1, p2, p3, p4, p5, p6, p7]
+print(myPlayerList)
