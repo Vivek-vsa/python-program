72fa2f2c1da71cb55fe2e4f7673273d68b7c7a53
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-13 10:21:02 +0530

    Dictionary_syntax.py

---
+++ .vscode/Dictionary_syntax.py
@@ -0,0 +1,13 @@
+myDict = {
+    "Fast": "In a Quick Manner",
+    "Vicky": "A Coder",
+    "Marks": [1, 2, 5],
+    "anotherdict": {'Vicky': 'Player'}
+}
+
+ #print(myDict["Fast"])
+ #print(myDict["Vicky"])
+ #update or change the data in dictionary
+myDict['Marks'] = [45, 78]
+print(myDict['Marks'])
+print(myDict['anotherdict']['Vicky'])
