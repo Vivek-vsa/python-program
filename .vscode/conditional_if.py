26f97c985c6ca39bd5412a4272bdeb4ed907cdac
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-14 12:16:32 +0530

    Multiple if statements

---
+++ .vscode/conditional_if.py
@@ -0,0 +1,18 @@
+a = 12
+
+#  Multiple if statements
+if(a<3): 
+    print("The value of a is greater than 3")
+
+if(a>13):
+    print("The value of a is greater than 13")
+    
+if(a>7):
+    print("The value of a is greater than 7")
+
+if(a>17):
+    print("The value of a is greater than 17")
+else:
+    print("The value is not greater than 3 or 7")
+
+print("Done")
