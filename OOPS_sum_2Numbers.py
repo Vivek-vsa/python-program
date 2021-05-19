2599efeeb2bda7d06d4970f200c20b5ca249f1a3
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-05-19 23:18:35 +0530

    OOPS concept

---
+++ OOPS_sum_2Numbers.py
@@ -0,0 +1,24 @@
+# oops concept code to write sum of two numbers
+class Number:
+    def sum(self):
+        return self.a + self.b
+
+num = Number()
+num.a = 12          #you can take this values from  inputs also
+num.b = 34
+s = num.sum()
+print("sum is equal to ",s)
+
+#normal code in python to add two numbers
+# a = 12
+# b = 34
+
+# print("The sum of a and b is ", a+b)
+
+'''
+PascalCase 
+EmployeeName -->PascalCase 
+
+camelCase
+isNumeric, isFloatOrInt -->camelCase
+'''
